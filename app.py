c8e226050e3cf5067ed7f89c401cec83c416bf91
qhung23125005 <xinquyhung91005@gmail.com>
2025-02-19 23:08:21 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,122 @@
+import os
+import re
+import string
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import streamlit as st
+from torchtext.data.utils import get_tokenizer
+from torchtext import vocab
+from torchtext.vocab import build_vocab_from_iterator, Vocab
+
+tokenizer = get_tokenizer("basic_english")
+idx2label = {0: 'negative', 1:'positive'}
+
+def preprocess_text(text):
+    # remove URLs https://www.
+    url_pattern = re.compile(r'https?://\s+\wwww\.\s+')
+    text = url_pattern.sub(r" ", text)
+
+    # remove HTML Tags: <>
+    html_pattern = re.compile(r'<[^<>]+>')
+    text = html_pattern.sub(" ", text)
+
+    # remove puncs and digits
+    replace_chars = list(string.punctuation + string.digits)
+    for char in replace_chars:
+        text = text.replace(char, " ")
+
+    # remove emoji
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U0001F1F2-\U0001F1F4"  # Macau flag
+        u"\U0001F1E6-\U0001F1FF"  # flags
+        u"\U0001F600-\U0001F64F"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U0001F1F2"
+        u"\U0001F1F4"
+        u"\U0001F620"
+        u"\u200d"
+        u"\u2640-\u2642"
+        "]+", flags=re.UNICODE)
+    text = emoji_pattern.sub(r" ", text)
+
+    # normalize whitespace
+    text = " ".join(text.split())
+
+    # lowercasing
+    text = text.lower()
+    return text
+
+class TextCNN(nn.Module):
+    def __init__(
+        self,
+        vocab_size, embedding_dim, kernel_sizes, num_filters, num_classes):
+        super(TextCNN, self).__init__()
+
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.kernel_sizes = kernel_sizes
+        self.num_filters = num_filters
+        self.num_classes = num_classes
+        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
+        self.conv = nn.ModuleList([
+            nn.Conv1d(
+                in_channels=embedding_dim,
+                out_channels=num_filters,
+                kernel_size=k,
+                stride=1
+            ) for k in kernel_sizes])
+        self.fc = nn.Linear(len(kernel_sizes) * num_filters, num_classes)
+
+    def forward(self, x):
+        batch_size, sequence_length = x.shape
+        x = self.embedding(x.T).transpose(1, 2)
+        x = [F.relu(conv(x)) for conv in self.conv]
+        x = [F.max_pool1d(c, c.size(-1)).squeeze(dim=-1) for c in x]
+        x = torch.cat(x, dim=1)
+        x = self.fc(x)
+        return x
+
+def load_model(model_path, vocab_size=10000, embedding_dim=100, num_classes=2):
+    model = TextCNN(
+        vocab_size=vocab_size,
+        embedding_dim=embedding_dim,
+        kernel_sizes=[3, 4, 5],
+        num_filters=100,
+        num_classes=num_classes
+    )
+    model.load_state_dict(torch.load(model_path, weights_only=True, map_location=torch.device('cpu')))
+    model.eval()
+    return model
+  
+model = load_model('textcnn.pt')
+vocabulary = torch.load('vocab.pth')
+
+def inference(sentence, vocabulary, model):
+    sentence = preprocess_text(sentence)
+    encoded_sentence = vocabulary(tokenizer(sentence))
+    encoded_sentence = torch.tensor(encoded_sentence)
+    encoded_sentence = torch.unsqueeze(encoded_sentence, 1)
+
+    with torch.no_grad():
+        predictions = model(encoded_sentence)
+    preds = nn.Softmax(dim=1)(predictions)
+    p_max, yhat = torch.max(preds.data, 1)
+    return round(p_max.item(), 2)*100, yhat.item()
+
+def main():
+  st.title('Sentiment Analysis')
+  st.title('Model: Text CNN. Dataset: NTC-SCV')
+  text_input = st.text_input("Sentence: ", "Đồ ăn ở quán này quá tệ luôn!")
+  p, idx = inference(text_input, vocabulary, model)
+  label = idx2label[idx]
+  st.success(f'Sentiment: {label} with {p:.2f} % probability.') 
+
+if __name__ == '__main__':
+     main() 
